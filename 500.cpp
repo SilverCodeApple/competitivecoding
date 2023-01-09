3b4d39634634cbe190511e7f335d5b7a743b191d
Simon Perez <simonsp12@hotmail.com>
2023-01-08 21:19:26 -0500

    primer codigo

--- 500.cpp
+++ 500.cpp
@@ -1,1 +1,24 @@
-#i
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+	double n = 0,
+	x , 
+	y ,
+	z ;
+	cin >> n;
+	while(n--){
+		cin>>x>>y>>z;
+		
+		if((x+y)/2 > z){
+			cout<<"YES"<<endl; 
+		}else{
+			cout<<"NO"<<endl;
+		}
+	}
+
+return 0;	
+}
