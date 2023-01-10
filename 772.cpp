38e70c9b847b18e350de277cbab1b48bdb297e0e
Simon Perez <simonsp12@hotmail.com>
2023-01-10 11:55:19 -0500

    new exercises

---
+++ 772.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int main(){
+   int t,a , b;
+   cin>>t;
+   while(t--){
+   	if((a+b)%2==0){
+   		
+	   }
+   }
+return 0;	
+}
---
+++ 800.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	int t, k , n;
+	
+	cin >> t;
+	
+	while(t--){
+		cin>>k>>n;
+		cout<<(k/n)*k<<endl;
+	}
+	
+	return 0;
+}
